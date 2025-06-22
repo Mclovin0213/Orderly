e5d71f03e12721ff18432cab65fa81eca9b33b43
Mclovin0213 <javellaneda0213@gmail.com>
2025-06-22 10:02:27 -0700

    Initial Commit

---
+++ Orderly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Orderly
+//
+//  Created by julian avellaneda on 6/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Orderly/OrderlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderlyApp.swift
+//  Orderly
+//
+//  Created by julian avellaneda on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct OrderlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
